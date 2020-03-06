ce75d27b0e89de261733c89750ab48f9afd556e7
= <dstadh17@gmail.com>
2020-03-06 18:09:47 +0800

    done with cvat exporter for yolo format

---
+++ cvat_exporter.py
@@ -0,0 +1,68 @@
+import shutil
+from PIL import Image
+from pathlib import Path
+
+class CVAT_exporter(object):
+    def __init__(self, upload_format='yolo', out_dir=None):
+        self.format = upload_format
+        if self.format == 'yolo':
+            self.write = self.write_yolo
+            self.end = self.end_yolo
+        else:
+            assert True,'Sorry currently only yolo is supported'
+        self.out_dir_path = Path(out_dir) 
+        self.out_dir_path.mkdir(exist_ok=True, parents=True)
+        self.classes = []
+        self.imnames = []
+        self.cvat_arbitary_dir_name = 'obj_train_data'
+        (self.out_dir_path/self.cvat_arbitary_dir_name).mkdir(exist_ok=True, parents=True) 
+
+    def write_yolo(self, impath, preds):
+        im = Image.open(impath)
+        iw, ih = im.size
+
+        impath = Path(impath)
+        # txt_fp = Path(str(impath).replace(impath.suffix, '.txt'))
+        txt_fp = self.out_dir_path / self.cvat_arbitary_dir_name / impath.name.replace(impath.suffix,'.txt')
+        # print('Writing {}'.format(txt_fp.name))
+        self.imnames.append(impath.name)
+        with open(txt_fp, 'w') as f:
+            for pred in preds:
+                bb, _, cl = pred
+                if cl not in self.classes:
+                    self.classes.append(cl)
+                class_idx = self.classes.index(cl)
+                l,t,r,b = bb
+                
+                w = r - l + 1
+                h = b - t + 1
+                cen_x = l + (r - l)/2
+                cen_y = t + (b - t)/2
+                
+                cen_x = cen_x / iw
+                cen_y = cen_y / ih
+                w = w/iw
+                h = h/ih
+
+                write_str = '{} {} {} {} {}\n'.format(class_idx, cen_x, cen_y, w, h)
+                f.write(write_str)
+                # print(write_str)
+
+    def end_yolo(self):
+        with (self.out_dir_path/'obj.data').open(mode='w') as f:
+            f.write('classes = {}\n'.format(len(self.classes)))
+            f.write('train = data/train.txt\n')
+            f.write('names = data/obj.names\n')
+            f.write('backup = backup/\n')
+        
+        with (self.out_dir_path/'obj.names').open(mode='w') as f:
+            for cl in self.classes:
+                f.write(cl+'\n')
+
+        with (self.out_dir_path/'train.txt').open(mode='w') as f:
+            for imname in self.imnames:
+                fake_path = Path('data') / self.cvat_arbitary_dir_name / imname  
+                f.write(str(fake_path)+'\n')
+
+        print('Zipping up..')
+        shutil.make_archive(str(self.out_dir_path),'zip',str(self.out_dir_path))`
---
+++ recommend_boxes.py
@@ -0,0 +1,74 @@
+import cv2
+import argparse
+from tqdm import tqdm
+from pathlib import Path
+from keras_yolo3.yolo import YOLO
+from cvat_exporter import CVAT_exporter
+
+IMG_EXTS = ['.png','.jpg','.jpeg']
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "img_dir", help="Directory containing images", type=str
+)
+parser.add_argument(
+    "--thresh", help="Object Detection threshold", type=float, default=0.1
+)
+parser.add_argument(
+    "--viz", help='flag to viz results', action='store_true')
+args = parser.parse_args()
+assert args.thresh > 0.0
+img_dir_path = Path(args.img_dir)
+assert img_dir_path.is_dir()
+
+def get_good_bbs(res):
+    good = []
+    for this_res in res:
+        bb, conf, cl = this_res
+        l,t,r,b = bb
+        if cl == 'ship' and conf > args.thresh:
+            good.append(this_res)
+    return good
+
+od = YOLO(
+    bgr=True,
+    gpu_usage=0.3,
+    score=0.05,
+    input_image_size=(10,10),
+    model_path="keras_yolo3/model_data/pp_reanchored_best_val.h5",
+    anchors_path="keras_yolo3/model_data/PP_ALL_anchors.txt",
+    classes_path="keras_yolo3/model_data/PP_classes.txt",
+)
+print('YOLO inited.')
+
+exporter = CVAT_exporter(upload_format='yolo', out_dir=str(img_dir_path)+'_preds')
+
+# for impath in img_dir_path.glob('**/*'):
+for impath in tqdm(list(img_dir_path.glob('**/*'))):
+    if not str(impath).endswith(tuple(IMG_EXTS)):
+        continue
+    img = cv2.imread(str(impath))
+
+    od.input_image_size=img.shape[:2]
+    
+    res = od.detect_get_box_in(
+        img,
+        box_format="ltrb",
+        classes=["ship"],
+        buffer_ratio=0.0,
+    )
+
+    good_res = get_good_bbs(res)
+    exporter.write(impath, good_res)
+
+    if args.viz:
+        show_frame = img.copy()
+        for this_res in good_res:
+            bb, conf, cl = this_res
+            l,t,r,b = bb
+            cv2.rectangle(show_frame, (l,t), (r,b), color=(255,255,0), thickness=2)
+        cv2.imshow(impath.name,show_frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+exporter.end()
