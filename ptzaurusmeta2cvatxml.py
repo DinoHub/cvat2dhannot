2a01bd08799e5a9a3e12bcdc35afb19b3e75f131
= <dstadh17@gmail.com>
2020-03-11 15:30:34 +0800

    ptzaurus predictions meta info convert to cvat xml done

--- ptzaurusmeta2cvatxml.py
+++ ptzaurusmeta2cvatxml.py
@@ -1,10 +1,11 @@
 import argparse
-from pathlib import Path
+import xml.etree.ElementTree as ET
+
 from tqdm import tqdm
+from pathlib import Path
 from collections import defaultdict
 
 CLASSES = ['ship']
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "meta_file", help="Path to meta file containing predictions", type=str
@@ -14,68 +15,83 @@ args = parser.parse_args()
 meta_file_path = Path(args.meta_file)
 assert meta_file_path.is_file()
 
-out_file = meta_file_path.parent.parent / '{}.csv'.format(meta_file_path.stem)
+out_file = meta_file_path.parent.parent / '{}.xml'.format(meta_file_path.stem)
 out_file_path = Path(out_file) 
 
 with meta_file_path.open() as f:
     lines = f.readlines()
 
-imgname2bbs = defaultdict(list)
+bb_count = 0
+imgnames = []
+track_ids = []
+track_idx2bbs = defaultdict(list)
+track_idx2classes = defaultdict(list)
 for line in lines:
     splits = line.split(' ')
     imgname = splits[0]
+    if imgname not in imgnames:
+        imgnames.append(imgname)
     bbs = splits[1:]
     for bb in bbs:
+        bb_count += 1
         l,t,r,b,class_idx,track_id,bigsmall_val = bb.split(',')
         pred = [[float(l),float(t),float(r),float(b)], CLASSES[int(class_idx)], track_id ]
-        imgname2bbs[imgname].append(pred)
+        if track_id not in track_ids:
+            track_ids.append(track_id)
+        track_idx = track_ids.index(track_id)
+        track_idx2bbs[track_idx].append([imgname, l, t, r, b])
+        track_idx2classes[track_idx].append(CLASSES[int(class_idx)])
 
-sorted_imgnames = sorted(imgname2bbs.keys(), key=lambda x: int(x.split('_')[-1].split('.')[0]))
-print(sorted_imgnames)
+sorted_imgnames = sorted(imgnames, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+# print(sorted_imgnames)
 
-'''
-The MOT CSV format
-Frame number (starts from 0), Track id (starts from 0), l, t, w, h, Confidence (default 1.0), Class (in string),Visibility ratio (how much of the object is visible, default is 1.0)
-'''
-bb_count = 0
-track_ids = []
-import cv2
-img_dir = '/media/dh/HDD/pluggy_archive/2020-03-10T00:07:31:481000'
-with out_file_path.open(mode='w') as f:
-    for frame_idx, imname in enumerate(sorted_imgnames):
-        print(frame_idx, imname)
-        impath = Path(img_dir) / imname
-        show_frame = cv2.imread(str(impath))
-        for pred in imgname2bbs[imname]:
-            assert len(pred) == 3
-            bb, cl, track_id = pred
-            print(track_id)
-            color=(255,255,0)
-            if track_id not in track_ids:
-                track_ids.append(track_id)
-                print('added', track_id)
-                color=(0,0,255)
+# XML WRITING
+# intro
+xml_data = ET.Element('annotations')
+version = ET.SubElement(xml_data, 'version')
+version.text = '1.1'
 
-            l,t,r,b = bb
-            
-            w = r - l + 1
-            h = b - t + 1
+# Writing the actual stuffs
+box_props = ['frame', 'outside', 'occluded', 'keyframe', 'xtl', 'ytl', 'xbr','ybr']
+outside = 0
+occluded = 0
+keyframe = 1
+for track_idx, track_id in enumerate(track_ids):
+    track_xml_element = ET.SubElement(xml_data, 'track')
+    track_xml_element.set('id', str(track_idx))
+    classes = track_idx2classes[track_idx]
+    mode_class = max(set(classes), key=class_idx.count) # the class that appear the most in the track
+    track_xml_element.set('label', mode_class)
+    last_frame_idx = -1
+    for box in track_idx2bbs[track_idx]:
+        box_xml_element = ET.SubElement(track_xml_element, 'box')
+        box_xml_element.text = " "
+        imgname, l, t, r, b = box
+        curr_frame_idx = sorted_imgnames.index(imgname)
+        res = [ curr_frame_idx, outside, occluded, keyframe, l, t, r, b ]
+        if curr_frame_idx > last_frame_idx:
+            last_frame_idx = curr_frame_idx
+        for prop, val in zip(box_props, res):
+            # print('Setting {} as {}'.format(prop, val))
+            box_xml_element.set(prop, str(val))
+    
+    # CVAT has a weird quirk, we need to 'end' a track by having an "outside=1" box at the next frame, the other properties of the box does not matter at all
+    last_last_frame_idx = last_frame_idx + 1
+    if last_last_frame_idx >= len(sorted_imgnames):
+        continue
+    end_box_element = ET.SubElement(track_xml_element, 'box')
+    end_box_element.text = " "
+    imgname, l, t, r, b = box
+    res = [ last_last_frame_idx, 1, occluded, keyframe, l, t, r, b ]
+    for prop, val in zip(box_props, res):
+        # print('Setting {} as {}'.format(prop, val))
+        end_box_element.set(prop, str(val))
 
-            track_idx = track_ids.index(track_id)
-            print('{}: {}'.format(track_id, track_idx))
+mydata = ET.tostring(xml_data, encoding='unicode')
+with out_file_path.open(mode="w") as f:
+    f.write(mydata)
 
-            write_str = '{},{},{},{},{},{},{},{},{}\n'.format(frame_idx, track_ids.index(track_id), l,t,w,h, 1.0, cl, 1.0 )
-            f.write(write_str)
-            # print(write_str)
-            bb_count += 1
-            cv2.rectangle(show_frame, (int(l),int(t)), (int(r),int(b)), color=color, thickness=2)
-            cv2.putText(show_frame, '{}:{}'.format(track_id, track_idx+1), (int(l),int(t)), cv2.FONT_HERSHEY_DUPLEX, 1.0, color, thickness=2)
-
-        # cv2.imshow('', show_frame)
-        # cv2.waitKey(0)
-        cv2.imwrite('{}.png'.format(frame_idx), show_frame)
-
-print(track_ids)
+print('XML outputed at {}'.format(out_file_path))
 print('Total num of frames: {}'.format(len(sorted_imgnames)))
 print('Total num of tracks: {}'.format(len(track_ids)))
 print('Total num of bbs: {}'.format(bb_count))
